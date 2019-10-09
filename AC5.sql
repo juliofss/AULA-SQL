ffbcc77a2a7219289c58284fce4fd1eebd48af10
juliofss <36732973+juliofss@users.noreply.github.com>
2019-10-09 13:37:47 -0300

    Adicionando arquivo da AC 5

---
+++ AC5.sql
@@ -0,0 +1,27 @@
+INSERT INTO Usuario (Login, Senha) VALUES ('Malu ', '1234'), ('Leonardo', 't1234'),('Laura', '1234'),('Bruna', '1234'),('Leandro', '1234'),('Benedito', '1234'),('Filipe', '1234')
+,('Lucia', '1234'),('Diego', '1234'),('Caua', '1234'),('Thiago', '1234'),('Daniela', '1234'),('Beatriz', '1234'),('Fabio', '1234'),('Eliane', '1234')
+
+INSERT INTO Coordenador (Id_usuario, Nome, Email, Celular) 
+VALUES (1, 'Malu Mirella Benedita Mendes', 'malumirellabeneditamendes@gmail.com', '62988313893'),
+		(2, 'Leonardo Cláudio da Mota', 'leonardoclaudiodamota@gmail.com', '62999389851'),
+		(3, 'Laura Vanessa Aragão', 'lauravanessaaragao@gmail.com', '45993153340'),
+		(4, 'Bruna Gabrielly Araújo', 'brunagabriellyaraujo@gmail.com', '92988763370'),
+		(5, 'Leandro Antonio Renan Dias', 'eandroantoniorenandias@gmail.com', '85993991833')
+
+
+INSERT INTO Aluno (Id_usuario, Nome, Email, Celular, RA) 
+VALUES (6, 'Benedito Anthony Almada','bbeneditoanthonyalmada@gmail.com','16994580975','1900001'),
+		(7, 'Filipe Ryan Souza','filiperyansouza-74@gmail.com','34987998225','1900002'),
+		(8, 'Lúcia Tânia Fernanda Porto','luciataniafernandaporto@gmail.com','51996368224','1900003'),
+		(9, 'Diego Hugo Benício Moraes','diegohugobeniciomoraes@gmail.com','65999903660','1900004'),
+		(10, 'Cauã Igor Santos','cauaigorsantos-70@gmail.com','27997679717','1900005')
+
+INSERT INTO Professor (Id_usuario, Email, Celular, Apelido)
+VALUES (11,'tthiagocarloscastro@gmail.com','47993503817','Thiago'),
+		(12,'danielaoliviamoraes_@gmail.com','96983278262','Daniela'),
+		(13,'beatrizsandraviana-98@gmail.com','98984198085','Beatriz'),
+		(14,'fabiotheogoncalves@gmail.com','51993895239','Fabio'),
+		(15,'elianemarianamoraes@gmail.com','67985119797','Eliane')
+
+INSERT INTO Disciplina (Nome, Data, PlanoDeEnsino, CargaHoraria, Competencias, Habilidades, Ementa, ConteudoProgramatico, BibliografiaBasica, BibliografiaComplementar,PercentualPratico, PercentualTeorico, IdCoordenador)
+VALUES ()
